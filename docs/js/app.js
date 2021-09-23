ec42a1dd3faf507686b1f3d01b43059c12d92074
airesucio <91238053+airesucio@users.noreply.github.com>
2021-09-23 00:55:22 -0300

    Add files via upload

---
+++ docs/js/app.js
@@ -0,0 +1,96 @@
+const errorLabel = document.querySelector("label[for='error-msg']")
+const latInp = document.querySelector("#latitude")
+const lonInp = document.querySelector("#longitude")
+const airQuality = document.querySelector(".air-quality")
+const airQualityStat = document.querySelector(".air-quality-status")
+const srchBtn = document.querySelector(".search-btn")
+const componentsEle = document.querySelectorAll(".component-val")
+
+const appId = 'd76ef65d65b5b2b6aa1247136644bef3'
+
+const link = "https://api.openweathermap.org/data/2.5/air_pollution"
+
+const getUserLocation = () => {
+	// Get user Location
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(onPositionGathered, onPositionGatherError)
+	} else {
+		onPositionGatherError({ message: "No podemos ingresar a tu locación, por favor ingrese sus cordenadas" })
+	}
+}
+
+const onPositionGathered = (pos) => {
+	let lat = pos.coords.latitude.toFixed(4), lon = pos.coords.longitude.toFixed(4)
+
+	// Set values of Input for user to know
+	latInp.value = lat
+	lonInp.value = lon
+
+	// Get Air data from weather API
+	getAirQuality(lat, lon)
+}
+
+const getAirQuality = async (lat, lon) => {
+	// Get data from api
+	const rawData = await fetch(`${link}?lat=${lat}&lon=${lon}&appid=${appId}`).catch(err => {
+		onPositionGatherError({ message: "Algo anda mal. Revisa tu conexión" })
+		console.log(err)
+	})
+	const airData = await rawData.json()
+	setValuesOfAir(airData)
+	setComponentsOfAir(airData)
+}
+
+const setValuesOfAir = airData => {
+	const aqi = airData.list[0].main.aqi
+	let airStat = "", color = ""
+
+	// Set Air Quality Index
+	airQuality.innerText = aqi
+
+	// Set status of air quality
+
+	switch (aqi) {
+		case 1:
+			airStat = "Excelente"
+			color = "rgb(19, 201, 28)"
+			break
+			case 2:
+				airStat = "Buena"
+				color = "rgb(15, 134, 25)"
+				break
+			case 3:
+				airStat = "Moderada"
+				color = "rgb(201, 204, 13)"
+				break
+			case 4:
+				airStat = "Pobre"
+				color = "rgb(204, 83, 13)"
+				break
+		case 5:
+			airStat = "Insalubre"
+			color = "rgb(204, 13, 13)"
+			break
+		default:
+			airStat = "Desconocida"
+	}
+
+	airQualityStat.innerText = airStat
+	airQualityStat.style.color = color
+}
+
+const setComponentsOfAir = airData => {
+	let components = {...airData.list[0].components}
+	componentsEle.forEach(ele => {
+		const attr = ele.getAttribute('data-comp')
+		ele.innerText = components[attr] += " μg/m³"
+	})
+}
+
+const onPositionGatherError = e => {
+	errorLabel.innerText = e.message
+}
+
+srchBtn.addEventListener("click", () => {
+	getAirQuality(parseFloat(latInp.value).toFixed(4), parseFloat(lonInp.value).toFixed(4))
+})
